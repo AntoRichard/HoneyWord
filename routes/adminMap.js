2f771e9c712511d9c301a0c98cc2d0c1c2f28057
Anto Richard <anto22998@gmail.com>
2019-03-26 18:47:07 +0530

    final

--- routes/adminMap.js
+++ routes/adminMap.js
@@ -1,9 +1,12 @@
-const express  =require('express');
+const express = require('express');
 
-const  route = express.Router();
+const route = express.Router();
 
-route.get('/adminMap',(req,res)=>{
-    res.render('adminMap');
+route.get('/adminMap', (req, res) => {
+    res.render('adminMap', {
+        lat : 12.8698817,
+        lng : 80.2162332
+    });
 });
 
 module.exports = route;
--- utils/login.js
+++ utils/login.js
@@ -1,7 +1,7 @@
 const hash = require('object-hash');
 const userModel = require('../database/user');
 const indexModel = require('../database/indexing');
-
+let count=0;
 exports.getLogin = (req, res) => {
     res.render('login');
 }
@@ -9,26 +9,33 @@ exports.getLogin = (req, res) => {
 exports.postLogin = (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
-    
-    if(username == null || password==null){
+    if(username === "admin" && password === "admin"){
+        res.redirect('/adminMap');
+    }else{
         // console.log('hello world');
-        let hashedPassword = hash.sha1(password);
+    let hashedPassword = hash.sha1(password);
     let index = 0;
-
     // console.log(`${username}\n${password}`);
     // console.log(username+"\n"+password);
     let data = {
         username: username
     }
+  
     indexModel.findOne(data, (err, result) => {
-        if (err) throw err;
+        if (err){
+            res.redirect('/signup');
+        }
+        
         // index = result.index;
         console.log(result.index);
         index = result.index;
     });
+
     console.log(index);
     userModel.findOne(data, (err, result) => {
-        if (err) throw err;
+        if (err){
+            res.redirect('/signup');
+        }
         console.log(result);
         let honeywords = [result.password1, result.password2, result.password3, result.password4, result.password5, result.password6, result.password7, result.password8];
         let foundPassword = honeywords[index-1];
@@ -39,12 +46,17 @@ exports.postLogin = (req, res) => {
             console.log(hashedPassword);
         }
         else{
-            res.redirect('/login');
-            console.log(foundPassword);
-            console.log(hashedPassword);
+            if(count<3)
+            {
+                res.redirect('/login');
+                count++;
+                console.log(count);
+            }else{
+                res.redirect('/dummy');
+                count=0;
+                console.log(count);
+            }
         }
     });
-    }else{
-        res.redirect('/login');
-    }
+}
 }
